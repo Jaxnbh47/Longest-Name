0a409e481bc60146b39b2f4927a6ae32e1b69302
Jackson Harris <109924053+Jaxnbh47@users.noreply.github.com>
2022-10-14 14:27:20 -0400

    master

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 let name1 = "Jackson";
 let name2 = "Brett";
+let name3 = "Harris";
+let num1 = name1.length;
+let num2 = name2.length;
+let num3 = name3.length;
 
-console.log(name1.length);
+if (num1 > num2 && num1 > num3){
+    console.log(`${name1} has the longest name.`)
+}
 
-console.log(name2.length);
+else if (num2 > num1 && num2 > num3){
+    console.log(`${name2} has the longest name.`)
+}
+
+else if (num3 > num1 && num3 > num2){
+    console.log(`${name3} has the longest name.`)
+}
+
+else if (num1 === num2 && num1 === num3){
+console.log(`${name1}, ${name2} and ${name3} are the same length.`)
+}
+
+else if (num1 === num2 && num1 > num3){
+    console.log(`${name1}, ${name2} tie for the longest name.`)
+}
+
+else if (num2 === num3 && num2 > num1){
+    console.log(`${name2}, ${name3} tie for the longest name.`)
+}
+
+else if (num1 === num3 && num1 > num2){
+    console.log(`${name1}, ${name3} tie for the longest name.`)
+}
+
